76af3509ccb8a8682b0d9e9c49e324431950a2d2
nxfive <kontakt@nxfive.pl>
2025-04-16 13:38:46 +0200

    feat: Switch model upload from MLflow to local models directory

--- project/src/project/server/export_model.py
+++ project/src/project/server/export_model.py
@@ -1,27 +1,28 @@
 import bentoml
-import mlflow
 import yaml
+import joblib
+from pathlib import Path
 
-with open('../../config/params.yaml', 'r') as f:
+config_path = (Path(__file__).resolve().parents[2]) / 'config/params.yaml'
+
+with open(config_path, 'r') as f:
     params = yaml.safe_load(f)
 
-mlflow.set_tracking_uri('file:../../mlruns')
 model_name = 'MedicalRegressor'
-
-sklearn_model = mlflow.sklearn.load_model(f'models:/{model_name}/latest')
-
 bentoml_model_name = params.get('bento', {}).get('model_name', 'medical_regressor')
 bentoml_model_tag = params.get('bento', {}).get('model_tag', 'latest')
 
+model_output = (Path(__file__).resolve().parents[3]) / 'models/medical_regressor.pkl'
+
 bento_model = bentoml.sklearn.save_model(
     name=f'{bentoml_model_name}:{bentoml_model_tag}',
-    model=sklearn_model,
+    model=joblib.load(model_output),
     signatures={
         'predict': {'batchable': True, 'batch_dim': 0}
     },
     metadata={
         'description': 'Random Forest Regressor for medical cost prediction',
-        'source': 'Mlflow Registry',
+        'source': 'project/models',
         'model_name': model_name
     }
 )
