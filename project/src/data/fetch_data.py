dbfd11c058af883cc361075d59873e7816ace429
nxfive <kontakt@nxfive.pl>
2025-05-12 22:29:30 +0200

    fix: Change paths for correct visibility in Gitlab CI

--- project/src/data/fetch_data.py
+++ project/src/data/fetch_data.py
@@ -4,13 +4,20 @@ import shutil
 from pathlib import Path
 
 
-cache_path = kagglehub.dataset_download('mirichoi0218/insurance')
-path = shutil.copytree(cache_path, '../../data/raw', dirs_exist_ok=True)
+def main():
+    cache_path = kagglehub.dataset_download('mirichoi0218/insurance')
+    print(cache_path)
+    path = shutil.copytree(cache_path, 'data/raw', dirs_exist_ok=True)
+    print(path)
 
-csv_file = next(Path(path).rglob('*.csv'), None)
-df = pd.read_csv(csv_file)
+    csv_file = next(Path(path).rglob('*.csv'), None)
+    df = pd.read_csv(csv_file)
 
-parquet_file_path = path + '/insurance.parquet'
-df.to_parquet(parquet_file_path, engine='pyarrow')
+    parquet_file_path = path + '/insurance.parquet'
+    df.to_parquet(parquet_file_path, engine='pyarrow')
 
-csv_file.unlink()
+    csv_file.unlink()
+
+
+if __name__ == '__main__':
+    main()
--- project/src/data/split_data.py
+++ project/src/data/split_data.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 import os
 
 
-df = pd.read_parquet('../../data/raw/insurance.parquet')
+df = pd.read_parquet('data/raw/insurance.parquet')
 
 X = df.drop(['charges'], axis=1)
 y = df['charges']
@@ -12,9 +12,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, train_size=0.2, random_state=42
 )
 
-os.makedirs('../../data/processed', exist_ok=True)
+os.makedirs('data/processed', exist_ok=True)
 
-X_train.to_csv('../../data/processed/X_train.csv', index=False)
-X_test.to_csv('../../data/processed/X_test.csv', index=False)
-y_train.to_csv('../../data/processed/y_train.csv', index=False)
-y_test.to_csv('../../data/processed/y_test.csv', index=False)
+X_train.to_csv('data/processed/X_train.csv', index=False)
+X_test.to_csv('data/processed/X_test.csv', index=False)
+y_train.to_csv('data/processed/y_train.csv', index=False)
+y_test.to_csv('data/processed/y_test.csv', index=False)
