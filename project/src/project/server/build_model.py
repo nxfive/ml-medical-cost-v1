8bc389db0f788a22fdad0b4738758828d3d71b0b
nxfive <kontakt@nxfive.pl>
2025-05-13 14:07:51 +0200

    fix: Change paths for correct visibility in Gitlab CI

--- project/src/project/server/build_model.py
+++ project/src/project/server/build_model.py
@@ -5,25 +5,25 @@ import joblib
 from pathlib import Path
 from datetime import datetime
 
+params_path = Path(__file__).resolve().parents[2] / 'config' / 'params.yaml'
 
-with open('../../config/params.yaml', 'r') as f:
+with open(params_path, 'r') as f:
     params = yaml.safe_load(f)
 
-mlflow.set_tracking_uri('file:../../mlruns')
+mlflow.set_tracking_uri('file:mlruns')
 model_name = 'MedicalRegressor'
 
 sklearn_model = mlflow.sklearn.load_model(f'models:/{model_name}/latest')
 
-model_tag = datetime.now().strftime("%Y%m%d%H%M%S")  
-model_output = (Path(__file__).resolve().parents[3]) / f'models/medical_regressor_{model_tag}.pkl'
+timestamp_tag = datetime.now().strftime("%Y%m%d%H%M%S") 
+model_output = (Path(__file__).resolve().parents[3]) / f'models/medical_regressor_{timestamp_tag}.pkl'
 
 joblib.dump(sklearn_model, model_output)
 
 bentoml_model_name = params.get('bento', {}).get('model_name', 'medical_regressor')
-bentoml_model_tag = params.get('bento', {}).get('model_tag', 'latest')
 
 bento_model = bentoml.sklearn.save_model(
-    name=f'{bentoml_model_name}:{bentoml_model_tag}',
+    name=f'{bentoml_model_name}:{timestamp_tag}',
     model=sklearn_model,
     signatures={
         'predict': {'batchable': True, 'batch_dim': 0}
