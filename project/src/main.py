c7cf5b4cc841018d0bff19bee32e5d5fc65ceb52
nxfive <kontakt@nxfive.pl>
2025-05-13 13:47:31 +0200

    fix: Change paths for correct visibility in Gitlab CI

--- project/src/main.py
+++ project/src/main.py
@@ -12,10 +12,10 @@ params_path = os.path.join(base_dir, 'config', 'params.yaml')
 with open(params_path, 'r') as f:
     params = yaml.safe_load(f)
 
-X_train = pd.read_csv('../../data/processed/X_train.csv')
-X_test = pd.read_csv('../../data/processed/X_test.csv')
-y_train = pd.read_csv('../../data/processed/y_train.csv').values.ravel()
-y_test = pd.read_csv('../../data/processed/y_test.csv').values.ravel()
+X_train = pd.read_csv('data/processed/X_train.csv')
+X_test = pd.read_csv('data/processed/X_test.csv')
+y_train = pd.read_csv('data/processed/y_train.csv').values.ravel()
+y_test = pd.read_csv('data/processed/y_test.csv').values.ravel()
 
 cv = KFold(
     n_splits=params['cv']['n_splits'], 
@@ -26,5 +26,3 @@ study, best_model, best_params = optimize_model(
     X_train, y_train, params['optuna'], params['model']['features'], cv
     )
 log_best_model(best_model, best_params, study, X_train, X_test, y_train, y_test)
-
-
